576e4634b024a232809e4e452c911ac156fb2630
tyler-morales <tyler.morales.dev@gmail.com>
2022-05-31 00:45:42 -0500

    Add title

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.css'
 import {Nav} from '../components/global/Nav'
 import Footer from '../components/Footer'
+import Head from 'next/head'
+
+// Add Split bee tracking
+import splitbee from '@splitbee/web'
+splitbee.init()
 
 function MyApp({Component, pageProps}) {
   return (
     <>
+      <Head>
+        <title>Pack A Punch Greens | Get your microgreens</title>
+      </Head>
       <Nav />
       <Component {...pageProps} />
       <Footer />

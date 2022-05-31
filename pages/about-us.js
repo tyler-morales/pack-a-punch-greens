4faee32b36f660dcdc3d988772ad4bcf9ba81bc9
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2022-05-31 00:22:46 -0500

    Add placeholder text (#6)

---
+++ pages/about-us.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <main className="my-20 m-auto max-w-7xl">
+      <h1 className="text-4xl text-center">About us Section comming soon 👀</h1>
+    </main>
+  )
+}

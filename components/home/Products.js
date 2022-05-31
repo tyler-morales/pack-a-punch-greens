c913d69190efab7a27c8cf061a77c4524aae40e3
tyler-morales <tyler.morales.dev@gmail.com>
2022-05-30 23:47:36 -0500

    Decrease margin on bottom

--- components/home/Products.js
+++ components/home/Products.js
@@ -5,7 +5,7 @@ import {AiOutlineRight} from 'react-icons/ai'
 
 export default function Products() {
   return (
-    <section className="m-auto my-32">
+    <section className="m-auto mt-32">
       <h2 className="text-5xl md:text-7xl max-w-7xl m-auto px-5">
         <span className="block">What</span>
         <span>we&#8217;re growing</span>

76e36829c592e23a2ac713bba23ef13ee7d033a7
tyler-morales <tyler.morales.dev@gmail.com>
2022-05-31 00:37:28 -0500

    Add new images

--- components/home/Products.js
+++ components/home/Products.js
@@ -16,7 +16,7 @@ export default function Products() {
         <div className="bg-green-100 flex justify-center px-6">
           <div className="w-[350px] py-20">
             <Image
-              src="/images/packaging.jpg"
+              src="/images/packaging-broccoli.png"
               width={300}
               height={300}
               layout="responsive"
@@ -25,7 +25,7 @@ export default function Products() {
             <h3 className="text-3xl font-serif bg-green-600 text-green-100 rounded-lg text-center mt-8 py-1">
               Broccoli
             </h3>
-            <Link href="/microgreens/broccoli">
+            <Link href="/microgreens">
               <a className="font-normal text-green-800 text-center mt-4 flex items-center justify-center group">
                 <span>More Details</span>
                 <AiOutlineRight className="group-hover:translate-x-2 transition-all" />
@@ -38,7 +38,7 @@ export default function Products() {
         <div className="bg-rose-100 flex justify-center px-6">
           <div className="w-[350px] py-20">
             <Image
-              src="/images/packaging.jpg"
+              src="/images/packaging-radish.png"
               width={300}
               height={300}
               layout="responsive"
@@ -47,7 +47,7 @@ export default function Products() {
             <h3 className="text-3xl font-serif bg-rose-600 text-rose-100 rounded-lg text-center mt-8 py-1">
               Radish
             </h3>
-            <Link href="/microgreens/radish">
+            <Link href="/microgreens">
               <a className="font-normal text-rose-800 text-center mt-4 flex items-center justify-center group">
                 <span>More Details</span>
                 <AiOutlineRight className="group-hover:translate-x-2 transition-all" />
@@ -60,7 +60,7 @@ export default function Products() {
         <div className="bg-green-200 flex justify-center px-6">
           <div className="w-[350px] py-20">
             <Image
-              src="/images/packaging.jpg"
+              src="/images/packaging-arugula.png"
               width={300}
               height={300}
               layout="responsive"
@@ -69,7 +69,7 @@ export default function Products() {
             <h3 className="text-3xl font-serif bg-green-700 text-green-200 rounded-lg text-center mt-8 py-1">
               Arugula
             </h3>
-            <Link href="/microgreens/radish">
+            <Link href="/microgreens">
               <a className="font-normal text-green-800 text-center mt-4 flex items-center justify-center group">
                 <span>More Details</span>
                 <AiOutlineRight className="group-hover:translate-x-2 transition-all" />

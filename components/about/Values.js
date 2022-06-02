e0d041eff163a1bd8478a87618f56e942fd1b337
tyler-morales <tyler.morales.dev@gmail.com>
2022-06-01 23:20:27 -0500

    Add values section

---
+++ components/about/Values.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Values() {
+  return (
+    <section className="my-20 md:my-40">
+      <h2 className="text-5xl md:text-7xl max-w-7xl m-auto px-5">
+        <span className="block">Our</span>
+        <span>Vision</span>
+      </h2>
+
+      {/* Value Cards */}
+      <div></div>
+    </section>
+  )
+}

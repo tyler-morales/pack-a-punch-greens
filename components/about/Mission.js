520f1bea56d26f3b0890208c255137f07471a0cc
tyler-morales <tyler.morales.dev@gmail.com>
2022-06-01 23:20:08 -0500

    Fix mission styling

---
+++ components/about/Mission.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Mission() {
+  return (
+    <section className="px-5 h-[50vh] md:h-[90vh] grid content-center bg-yellow-100">
+      <div className="max-w-7xl m-auto">
+        <h1 className="text-2xl text-center font-serif border-b-2 border-brand w-max m-auto">
+          Our Mission
+        </h1>
+        <p className="font-medium mt-8 text-xl text-center md:text-4xl leading-normal md:leading-relaxed">
+          Providing highly nutritious microgreens at affordable prices in local
+          communities through our dedication to efficient, sustainable, and
+          responsible environmental practices.
+        </p>
+      </div>
+    </section>
+  )
+}

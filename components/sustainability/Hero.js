8cfe96b2b1544299e01803c748e590c667e7629d
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2022-06-06 14:15:55 -0500

    ⭐️ Feature/sustainability page (#10)
    * remove dead code
    
    * Fix mission styling
    
    * Add about page
    
    * Add values section
    
    * Add grid columns to vision
    
    * Add ids to sections
    
    * Add correct ids to footer
    
    * Add content
    
    * Fix responsiveness

---
+++ components/sustainability/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 items-center mt-10">
+      <h1 className="text-brand md:text-center text-5xl md:text-7xl">
+        Sustainability
+      </h1>
+      <div className="">
+        <p className="font-medium mt-8">
+          Maintaining a sustainable culture environment through a circular
+          economy
+        </p>
+        <p className="font-medium mt-2">
+          Below are three key areas where we believe sustainable practices are
+          critical in a circular economy
+        </p>
+      </div>
+    </div>
+  )
+}

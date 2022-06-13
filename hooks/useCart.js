bbd27c47b67dc76f728652f7ceef4e6bfe1c57ec
tyler-morales <tyler.morales.dev@gmail.com>
2022-06-13 10:49:17 -0500

    Add a custom hook for cart

---
+++ hooks/useCart.js
@@ -0,0 +1,72 @@
+import {useState, createContext, useContext} from 'react'
+
+import {initiateCheckout} from '../lib/payments'
+
+import products from '../data/products.json'
+
+const defaultCart = {
+  products: {},
+}
+
+export const CartContext = createContext()
+
+export function useCartState() {
+  const [cart, updateCart] = useState(defaultCart)
+
+  const cartItems = Object.keys(cart.products).map((key) => {
+    const product = products.find(({id}) => `${id}` === `${key}`)
+    return {
+      ...cart.products[key],
+      pricePerUnit: product.price,
+    }
+  })
+
+  const subtotal = cartItems.reduce((accumulator, {pricePerUnit, quantity}) => {
+    return accumulator + pricePerUnit * quantity
+  }, 0)
+
+  const quantity = cartItems.reduce((accumulator, {quantity}) => {
+    return accumulator + quantity
+  }, 0)
+
+  function addToCart({id}) {
+    updateCart((prev) => {
+      let cart = {...prev}
+
+      if (cart.products[id]) {
+        cart.products[id].quantity = cart.products[id].quantity + 1
+      } else {
+        cart.products[id] = {
+          id,
+          quantity: 1,
+        }
+      }
+
+      return cart
+    })
+  }
+
+  function checkout() {
+    initiateCheckout({
+      lineItems: cartItems.map(({id, quantity}) => {
+        return {
+          price: id,
+          quantity,
+        }
+      }),
+    })
+  }
+
+  return {
+    cart,
+    subtotal,
+    quantity,
+    addToCart,
+    checkout,
+  }
+}
+
+export function useCart() {
+  const cart = useContext(CartContext)
+  return cart
+}
--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,24 @@ import {Nav} from '../components/global/Nav'
 import Footer from '../components/Footer'
 import Head from 'next/head'
 
+import {CartContext, useCartState} from '../hooks/useCart'
+
 // Add Split bee tracking
 import splitbee from '@splitbee/web'
 splitbee.init()
 
 function MyApp({Component, pageProps}) {
+  const cart = useCartState()
+
   return (
     <>
       <Head>
         <title>Pack A Punch Greens | Get your microgreens</title>
       </Head>
-      <Nav />
-      <Component {...pageProps} />
+      <CartContext.Provider value={{cart}}>
+        <Nav />
+        <Component {...pageProps} />
+      </CartContext.Provider>
       <Footer />
     </>
   )
